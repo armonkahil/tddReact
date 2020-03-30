f0dfccd1ca89d8474b7c4964aab9c1540cb84f56
armonkahil <armonkahil@gmail.com>
2020-03-30 06:02:54 -0400

    :memo: Build storybook docs

---
+++ docs/main.ce7372dd6fa05b22b51c.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,c){c(413),c(541),n.exports=c(867)},1:function(n,o){},541:function(n,o,c){"use strict";c.r(o);c(542),c(741)}},[[0,1,2]]]);
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,12 +1,17 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { withKnobs, number } from '@storybook/addon-knobs';
+import Carousel from '../src/Carousel';
+import slides from '../example/slides';
 
-import Carousel from '../src/Carousel'
-import slides from '../example/slides'
+const stories = storiesOf('Carousel', module);
+stories.addDecorator(withKnobs);
 
-storiesOf('Carousel', module).add('default', ()=> (
-  <Carousel slides={slides} />
-))
+stories.add('default', () => (
+  <Carousel
+    autoAdvanceDelay={number('autoAdvanceDelay', 10e3)}
+    slides={slides}
+    onIndexChange={action('onIndexChange')}
+  />
+));
